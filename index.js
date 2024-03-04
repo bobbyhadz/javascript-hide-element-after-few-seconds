661d65ed0f18dc4953beca0892d0464b6a9a44ed
bobbyhadz <opmftw@gmail.com>
2024-03-04 19:15:09 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('bobbyhadz.com');
+
+setTimeout(() => {
+  const box = document.getElementById('box');
+
+  // 👇️ removes element from DOM
+  box.style.display = 'none';
+
+  // 👇️ hides element (still takes up space on page)
+  // box.style.visibility = 'hidden';
+}, 1000); // 👈️ time in milliseconds
